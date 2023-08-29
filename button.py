908f2065f4e34133a468aff18800dcc00c9c9a40
saani <deishini2003@gmail.com>
2023-08-29 17:07:06 +0100

    updated version

--- button.py
+++ button.py
@@ -1,21 +1,31 @@
+import requests
 from gpiozero import LED, Button
 from signal import pause
 
 led = LED(17)
 button = Button(2)
-
-button.when_pressed = led.on
-button.when_released = led.off
+url = "https://maker.ifttt.com/trigger/button_pressed/json/with/key/emIUu4JtDzBTg5kilS4sKYLe8xWXjO-pKDCaPwWFgWB"
+# button.when_pressed = led.on
+# button.when_released = led.off
 
 from subprocess import call
 
 def print_thing():
-    print("reset")
+    print("button pressed")
     
-
- = print_thing
-led_3.off()
-   
+def send_request(url):
+    print("Hello")
+    r = requests.get(url)
+    if r.status_code == "200":
+        return led.on
+while True:   
+    if button.is_pressed:
+       
+        send_request(url)
+    
+#  
+# button.when_pressed = send_request()
+      
 pause()
 
 
